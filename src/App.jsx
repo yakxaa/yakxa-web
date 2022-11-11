efe050cb140984335a65d5e55be9100135e232fc
millouu <srishti.chaurasia123@gmail.com>
2022-11-11 22:59:35 +0530

    init: define folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   // const [count, setCount] = useState(0)
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
