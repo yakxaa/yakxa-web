b22058afadddb56affa4579cb1a3f6301025cb03
Suryakant Agrawal <72617587+suryaa62@users.noreply.github.com>
2022-11-11 23:24:13 +0530

    add: colors constant

---
+++ src/contant.js
@@ -0,0 +1,9 @@
+const backgroundColor = "FFFFFF";
+const contrastColor = "000000";
+const accentColorOne = "A7F393";
+const accentColorTwo = "FDA0B9";
+const accentColorThree = "FFCF77";
+const accentColorFour = "E9BEF9";
+const accentColorFive = "9DDADB";
+
+export default { backgroundColor , contrastColor , accentColorFive ,accentColorFour , accentColorOne , accentColorTwo , accentColorThree};
