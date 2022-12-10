c9551a0cf83c11232c8a1bcc5ee8da00aa9902ee
millouu <srishti.chaurasia123@gmail.com>
2022-12-10 21:44:30 +0530

    fix: landing

--- src/layouts/landing.jsx
+++ src/layouts/landing.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./navbar";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Landing = () => {
   return (
     <div className='min-h-screen flex flex-col justify-between '>
       <Navbar />
       <div>
-        <p className='font-semibold text-6xl'>Go Global</p>
-        <p className='text-5xl font-medium'>with AI dubbing!</p>
+        <p className='md:mb-2 text-4xl font-semibold md:text-6xl'>Go Global</p>
+        <p className='text-3xl md:text-5xl font-normal'>with AI dubbing!</p>
         <button
           className='bg-black border-2 border-black text-white py-2 px-4 my-8 rounded-none hover:border-black hover:cursor-pointer hover:bg-white hover:text-black hover:scale-110 transition-all ease-linear duration-300 '
           onClick={() => {
@@ -17,7 +17,12 @@ const Landing = () => {
           Get Demo
         </button>
       </div>
-      <LazyLoadImage src='assets\landing.png' className='w-full h-52 object-cover' placeholderSrc="assets\landing_ph.jpg" effect="blur"/>
+      <LazyLoadImage
+        src='assets\landing.png'
+        className='w-full h-52 object-cover'
+        placeholderSrc='assets\landing_ph.jpg'
+        effect='blur'
+      />
     </div>
   );
 };
