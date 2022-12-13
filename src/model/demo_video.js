c8b55f54fb248ba1fa99d896f52f03f05b19dedf
Suryakant Agrawal <72617587+suryaa62@users.noreply.github.com>
2022-12-14 02:49:03 +0530

    add : video links

--- src/model/demo_video.js
+++ src/model/demo_video.js
@@ -6,50 +6,112 @@ const links = {
 	"Voice Over" : {
 		"Original" : {
 			"Male": "https://www.youtube.com/embed/DAOFpgocfrg",
+			"Female" : "https://www.youtube.com/embed/DAOFpgocfrg"
 		},
 		"Bangla" : {
 			"Male": "https://www.youtube.com/embed/t50Vos47Bnw",
+			"Female" : "https://www.youtube.com/embed/6kfTyFfSULc"
 		},
 		"Gujrati" : {
 			"Male": "https://www.youtube.com/embed/yT3yd5X6rE4",
+			"Female" : "https://www.youtube.com/embed/RCJ-gYV9lNI"
 		},
 		"Hindi" : {
 			"Male": "https://www.youtube.com/embed/VVwthmzZqvw",
+			"Female" : "https://www.youtube.com/embed/DAOFpgocfrg"
 		},
 		"Kannada" : {
 			"Male": "https://www.youtube.com/embed/eJeG6SshbJk",
+			"Female" : "https://www.youtube.com/embed/RRDRX2km9n0"
 		},
 		"Malayalam" : {
 			"Male": "https://www.youtube.com/embed/zqAKrnetCuw",
+			"Female" : "https://www.youtube.com/embed/jbp5ulf-qgc"
 		},
 		"Marathi" : {
 			"Male": "https://www.youtube.com/embed/GrHRHLiyXQQ",
+			"Female" : "https://www.youtube.com/embed/dnagfc26TVI"
 		},
 		"Punjabi" : {
 			"Male": "https://www.youtube.com/embed/7Z3jR7hrhec",
+			"Female" : "https://www.youtube.com/embed/O4AYVmDWDXk"
 		},
 		"Tamil" : {
 			"Male": "https://www.youtube.com/embed/upK6QWtrNCY",
+			"Female" : "https://www.youtube.com/embed/Mo2APLKhxaQ"
 		},
 		"French" : {
 			"Male": "https://www.youtube.com/embed/ptuF2sSY1YE",
+			"Female" : "https://www.youtube.com/embed/DAOFpgocfrg"
 		},
 		"German" : {
 			"Male": "https://www.youtube.com/embed/qT_TIhugkm0",
+			"Female" : "https://www.youtube.com/embed/WYfZy-OHZ4M"
 		},
 		"Italian" : {
 			"Male": "https://www.youtube.com/embed/G-dVo6kFSAA",
+			"Female" : "https://www.youtube.com/embed/zHYyC4wesig"
 		},
 		"Japenese" : {
 			"Female": "https://www.youtube.com/embed/CFICt-0RQ8o",
 		},
 		"Russian" : {
 			"Male": "https://www.youtube.com/embed/BWh4PzOjtps",
+			"Female" : "https://www.youtube.com/embed/iCvo32swNLI"
 		},
 		"Spanish" : {
 			"Male": "https://www.youtube.com/embed/Q0RVEYkwZTE",
+			"Female" : "https://www.youtube.com/embed/DAOFpgocfrg"
 		},
-	}
+	},
+
+	"Animation" : {
+		"Original" : {
+			"Male": "https://www.youtube.com/embed/599gf4HPc2A",
+		},
+		"Bangla" : {
+			"Male": "https://www.youtube.com/embed/eLeFUA6cmw4",
+		},
+		"Gujrati" : {
+			"Male": "https://www.youtube.com/embed/oRGJtHaCFIM",
+		},
+		"Hindi" : {
+			"Male": "https://www.youtube.com/embed/HbD8AQ3QGBQ",
+		},
+		"Kannada" : {
+			"Male": "https://www.youtube.com/embed/1068gYtFkfE",
+		},
+		"Malayalam" : {
+			"Male": "https://www.youtube.com/embed/fMWfRrBXMag",
+		},
+		"Marathi" : {
+			"Male": "https://www.youtube.com/embed/gsYKGtpooFE",
+		},
+		"Punjabi" : {
+			"Male": "https://www.youtube.com/embed/1vHY0AHuFn4",
+		},
+		"Tamil" : {
+			"Male": "https://www.youtube.com/embed/19wkxchD91Y",
+		},
+		"French" : {
+			"Male": "https://www.youtube.com/embed/SR04kwi3-HE",
+		},
+		"German" : {
+			"Male": "https://www.youtube.com/embed/PfUsyQwbD10",
+		},
+		"Italian" : {
+			"Male": "https://www.youtube.com/embed/a5H0rZ7t_uU",
+		},
+		"Japenese" : {
+			"Female": "https://www.youtube.com/embed/zxA4EVkKeq0",
+		},
+		"Russian" : {
+			"Male": "https://www.youtube.com/embed/Y3872ynzLQg",
+		},
+		"Spanish" : {
+			"Male": "https://www.youtube.com/embed/nIiTry2vZGw",
+		},
+	},
 };
 
 export {languages , voice , categories ,links};
